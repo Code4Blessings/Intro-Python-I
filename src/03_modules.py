ff5aff6140b5b821fdb6a72729ae360208bd18f2
Code4Blessings <robinwarden@code4blessings.com>
2020-04-08 18:41:50 -0400

    Completed files 06-10

--- src/03_modules.py
+++ src/03_modules.py
@@ -16,10 +16,11 @@ for x in sys.argv:
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print(sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print(sys.version_info.major)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
@@ -30,6 +31,8 @@ print(os.getpgid(0))
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print(os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
